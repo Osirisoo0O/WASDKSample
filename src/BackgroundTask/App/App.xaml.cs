a1beeb07c7239fc22396b10782312e49196b7239
Osirisoo0O <setzero@live.cn>
2024-04-01 21:53:13 +0800

    Change root namespace

--- src/BackgroundTask/App/App.xaml.cs
+++ src/BackgroundTask/App/App.xaml.cs
@@ -20,7 +20,7 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace WASDKSample
+namespace App
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
